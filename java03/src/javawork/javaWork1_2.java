52adf089786091b573f867c712cbf2f86f300e18
Ylianqiao <1976450834@qq.com>
2018-05-02 15:48:15 +0800

    作业

--- java03/src/javawork/javaWork1_2.java
+++ java03/src/javawork/javaWork1_2.java
@@ -84,15 +84,27 @@ public class javaWork1_2 {
 		/*
 		double a=2.5;
 		int day1=0;
-		double c = 0;
+		double money = 0;
 		while(c<100) {
-			c=c+a;
+			money=money+a;
 			day1++;
 			if(day1%5==0) {
 				System.out.print("今天拿出了6元，");
-				c=c-6;
-			}System.out.println("第"+day1+"天存下"+c+"元");
+				money=money-6;
+			}System.out.println("第"+day1+"天存下"+money+"元");
 		}System.out.println("需要经过"+day1+"天才能存上一百元。");	
 		*/
+		
+		//求一百以内的所有偶数
+		int num=1;
+		while(num<=100){
+			if(num%2==0) {
+				System.out.print(num+"  ");
+			}
+			if(num%10==0) {
+				System.out.println("");
+			}
+			num++;
+		}
 	}
 }
--- java04/src/java04/javawork.java
+++
@@ -1,8 +0,0 @@
-package java04;
-
-public class javawork {
-	public static void main(String[] args) {
-		
-	}
-
-}
