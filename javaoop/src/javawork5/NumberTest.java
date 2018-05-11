27c5d43db407b643acfb70841a9a0ce413a5bc9a
Ylianqiao <1976450834@qq.com>
2018-05-11 17:01:34 +0800

    作业

--- javaoop/src/javawork5/NumberTest.java
+++ javaoop/src/javawork5/NumberTest.java
@@ -3,7 +3,7 @@ package javawork5;
 public class NumberTest {
 	public static void main(String[] args) {
 		Number num=new Number();
-		System.out.println();
+		System.out.println(num.addition());
 	}
 
 }
